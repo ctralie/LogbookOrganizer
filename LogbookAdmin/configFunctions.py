fd53687513b8de178ed66a76bcc854aafff42a7b
Roger <rogerzou94@gmail.com>
2014-07-29 15:47:19 -0400

    comment up changes

--- LogbookAdmin/configFunctions.py
+++ LogbookAdmin/configFunctions.py
@@ -1,5 +1,8 @@
 import json
 
+### Functions to read config.json ###
+
+# retrieve list of ignored files
 def getIgnoredFiles():
   json_data = open("config.json")
   configData = json.load(json_data)
